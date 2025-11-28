e89c8192ae57c61772260c3bfcaf616611856eae
chuka111 <118300161+chuka111@users.noreply.github.com>
2025-11-28 11:28:55 +0000

    updated workflow

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
-from fastapi import FastAPI, Depends, HTTPException, status
-from sqlalchemy.orm import Session
-from sqlalchemy import select
-from sqlalchemy.exc import IntegrityError
+from fastapi import FastAPI
 
-from app.database import engine, SessionLocal
-from app.models import Base, UserDB
-from app.schemas import UserCreate, UserRead
+app = FastAPI(title="Service A - Greeting API")
 
-app = FastAPI()
-Base.metadata.create_all(bind=engine)
 
 @app.get("/health")
 def health():
